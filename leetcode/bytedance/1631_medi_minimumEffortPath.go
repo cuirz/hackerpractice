84f4e035bf1b3a15f0193591c47e906549464293
cuirz <18686224@qq.com>
2021-01-30 13:36:22 +0800

    practice

---
+++ leetcode/bytedance/1631_medi_minimumEffortPath.go
@@ -0,0 +1,90 @@
+package main
+
+import "sort"
+
+//1631. 最小体力消耗路径
+//你准备参加一场远足活动。给你一个二维 rows x columns 的地图 heights ，其中 heights[row][col] 表示格子 (row, col) 的高度。一开始你在最左上角的格子 (0, 0) ，且你希望去最右下角的格子 (rows-1, columns-1) （注意下标从 0 开始编号）。你每次可以往 上，下，左，右 四个方向之一移动，你想要找到耗费 体力 最小的一条路径。
+//
+//一条路径耗费的 体力值 是路径上相邻格子之间 高度差绝对值 的 最大值 决定的。
+//
+//请你返回从左上角走到右下角的最小 体力消耗值 。
+//
+//输入：heights = [[1,2,2],[3,8,2],[5,3,5]]
+//输出：2
+//解释：路径 [1,3,5,3,5] 连续格子的差值绝对值最大为 2 。
+//这条路径比路径 [1,2,2,2,5] 更优，因为另一条路径差值最大值为 3 。
+//提示：
+//
+//rows == heights.length
+//columns == heights[i].length
+//1 <= rows, columns <= 100
+//1 <= heights[i][j] <= 106
+
+//思路 并查集  二分查找 最短路径
+// 二分查找，指定格子差最大值进行bfs或dfs寻路 (1~106)
+// 并查集
+func minimumEffortPath(heights [][]int) int {
+	n := len(heights)
+	m := len(heights[0])
+	parent := make([]int, n*m)
+	size := make([]int, n*m)
+	edges := make([][]int, 0)
+	for i := range parent {
+		parent[i] = i
+		size[i] = 1
+	}
+	var find func(x int) int
+	find = func(x int) int {
+		if parent[x] != x {
+			parent[x] = find(parent[x])
+		}
+		return parent[x]
+	}
+	union := func(x, y int) bool {
+		x, y = find(x), find(y)
+		if x == y {
+			return false
+		}
+		if size[x] < size[y] {
+			x, y = y, x
+		}
+		size[x] += size[y]
+		parent[y] = x
+		return true
+	}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			index := i*n + j
+			if i > 0 {
+				edges = append(edges, []int{index - m, index, abs(heights[i][j] - heights[i-1][j])})
+			}
+			if j > 0 {
+				edges = append(edges, []int{index - 1, index, abs(heights[i][j] - heights[i][j-1])})
+			}
+		}
+	}
+	sort.Slice(edges, func(i, j int) bool {
+		return edges[i][2] < edges[j][2]
+	})
+
+	end := n*m - 1
+	for _, v := range edges {
+		union(v[0], v[1])
+		if find(0) == find(end) {
+			return v[2]
+		}
+	}
+	return 0
+
+}
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func main(){
+	minimumEffortPath([][]int{{1,2,2},{3,8,2},{5,3,5}})
+}
---
+++ leetcode/bytedance/778_hard_swimInWater.go
@@ -0,0 +1,93 @@
+package main
+
+//778. 水位上升的泳池中游泳
+//在一个 N x N 的坐标方格 grid 中，每一个方格的值 grid[i][j] 表示在位置 (i,j) 的平台高度。
+//
+//现在开始下雨了。当时间为 t 时，此时雨水导致水池中任意位置的水位为 t 。你可以从一个平台游向四周相邻的任意一个平台，但是前提是此时水位必须同时淹没这两个平台。假定你可以瞬间移动无限距离，也就是默认在方格内部游动是不耗时的。当然，在你游泳的时候你必须待在坐标方格里面。
+//
+//你从坐标方格的左上平台 (0，0) 出发。最少耗时多久你才能到达坐标方格的右下平台 (N-1, N-1)？
+//
+// 
+//
+//示例 1:
+//
+//输入: [[0,2],[1,3]]
+//输出: 3
+//解释:
+//时间为0时，你位于坐标方格的位置为 (0, 0)。
+//此时你不能游向任意方向，因为四个相邻方向平台的高度都大于当前时间为 0 时的水位。
+//
+//等时间到达 3 时，你才可以游向平台 (1, 1). 因为此时的水位是 3，坐标方格中的平台没有比水位 3 更高的，所以你可以游向坐标方格中的任意位置
+//示例2:
+//
+//输入: [[0,1,2,3,4],[24,23,22,21,5],[12,13,14,15,16],[11,17,18,19,20],[10,9,8,7,6]]
+//输出: 16
+//解释:
+//0  1  2  3  4
+//24 23 22 21  5
+//12 13 14 15 16
+//11 17 18 19 20
+//10  9  8  7  6
+//
+//最终的路线用加粗进行了标记。
+//我们必须等到时间为 16，此时才能保证平台 (0, 0) 和 (4, 4) 是连通的
+// 
+//
+//提示:
+//
+//2 <= N <= 50.
+//grid[i][j] 是 [0, ..., N*N - 1] 的排列。
+
+//思路 并查集, 最短路径
+func swimInWater(grid [][]int) int {
+	n := len(grid)
+	parent := make([]int, n*n)
+	size := make([]int, n*n)
+	for i := range parent {
+		parent[i] = i
+		size[i] = 1
+	}
+	var find func(x int) int
+	find = func(x int) int {
+		if parent[x] != x {
+			parent[x] = find(parent[x])
+		}
+		return parent[x]
+	}
+	union := func(x, y int) {
+		x, y = find(x), find(y)
+		if x == y {
+			return
+		}
+		if size[x] < size[y] {
+			x, y = y, x
+		}
+		size[x] += size[y]
+		parent[y] = x
+	}
+
+	type point struct {
+		x, y int
+	}
+	pos := make([]point, n*n)
+	for i, row := range grid {
+		for j, h := range row {
+			pos[h] = point{i, j}
+		}
+	}
+
+	dir := []int{-1, 0, 1, 0, -1}
+	for h := 0; ; h++ {
+		for i := 0; i < 4; i++ {
+			x := pos[h].x + dir[i]
+			y := pos[h].y + dir[i+1]
+			if x >= 0 && y >= 0 && x < n && y < n && grid[x][y] <= h{
+				union(pos[h].x*n+pos[h].y, x*n+y)
+			}
+			if find(0) == find(n*n-1){
+				return h
+			}
+		}
+	}
+	return -1
+}
