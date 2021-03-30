1147548856845e27f09e053f6e675017ba84fc2c
cuirz <18686224@qq.com>
2021-03-30 13:39:02 +0800

    practice

---
+++ leetcode/bytedance/74_medi_searchMatrix.go
@@ -0,0 +1,37 @@
+package main
+
+import "sort"
+
+//74. 搜索二维矩阵
+//编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+//
+//每行中的整数从左到右按升序排列。
+//每行的第一个整数大于前一行的最后一个整数。
+//输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+//输出：true
+//输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+//输出：false
+//提示：
+//
+//m == matrix.length
+//n == matrix[i].length
+//1 <= m, n <= 100
+//-104 <= matrix[i][j], target <= 104
+
+//思路  二分查找
+func searchMatrix(matrix [][]int, target int) bool {
+	row := sort.Search(len(matrix), func(i int) bool {
+		return matrix[i][0] > target
+	}) - 1
+	col := sort.SearchInts(matrix[row], target)
+	return col < len(matrix[row]) && matrix[row][col] == target
+}
+
+func searchMatrix2(matrix [][]int, target int) bool {
+	m := len(matrix)
+	n := len(matrix[0])
+	index := sort.Search(m*n, func(i int) bool {
+		return matrix[i/n][i%n] >= target
+	})
+	return index < m*n && matrix[index/n][index%n] == target
+}
