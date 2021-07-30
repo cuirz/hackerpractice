0bbe331b0fb15e06a88c78572c8895856af9a14b
cuirz <18686224@qq.com>
2021-07-30 10:22:50 +0800

    practice

---
+++ leetcode/bytedance/171_easy_titleToNumber.go
@@ -0,0 +1,67 @@
+package main
+
+//171. Excel表列序号
+//给你一个字符串 columnTitle ，表示 Excel 表格中的列名称。返回该列名称对应的列序号。
+//
+// 
+//
+//例如，
+//
+//A -> 1
+//B -> 2
+//C -> 3
+//...
+//Z -> 26
+//AA -> 27
+//AB -> 28
+//...
+// 
+//
+//示例 1:
+//
+//输入: columnTitle = "A"
+//输出: 1
+//示例 2:
+//
+//输入: columnTitle = "AB"
+//输出: 28
+//示例 3:
+//
+//输入: columnTitle = "ZY"
+//输出: 701
+//示例 4:
+//
+//输入: columnTitle = "FXSHRXW"
+//输出: 2147483647
+// 
+//
+//提示：
+//
+//1 <= columnTitle.length <= 7
+//columnTitle 仅由大写英文组成
+//columnTitle 在范围 ["A", "FXSHRXW"] 内
+
+func titleToNumber(columnTitle string) int {
+	n := len(columnTitle)
+	var pow func(m, n int) int
+	pow = func(m, n int) int {
+		if n == 0 {
+			return 1
+		}
+		half := pow(m, n/2)
+		if n%2 == 0 {
+			return half * half
+		} else {
+			return half * half * m
+		}
+	}
+	var result int
+	for i := 0; i < n; i++ {
+		result += int(columnTitle[n-1-i]-'A'+1) * pow(26, i)
+	}
+	return result
+}
+
+func main() {
+	println(titleToNumber("AB"))
+}
