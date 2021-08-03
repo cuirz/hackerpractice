c86a8851c56634158886b3648a1eef39fc00efae
cuirz <18686224@qq.com>
2021-08-03 10:36:02 +0800

    practice

--- leetcode/bytedance/581_easy_findUnsortedSubarray.go
+++ leetcode/bytedance/581_easy_findUnsortedSubarray.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 //581. 最短无序连续子数组
 //给定一个整数数组，你需要寻找一个连续的子数组，如果对这个子数组进行升序排序，那么整个数组都会变为升序排序。
 //
@@ -14,8 +16,29 @@ package main
 //
 //输入的数组长度范围在 [1, 10,000]。
 //输入的数组可能包含重复元素 ，所以升序的意思是<=。
-
 func findUnsortedSubarray(nums []int) int {
+	minv, maxv := math.MaxInt32, math.MinInt32
+	l, r := -1, -1
+	n := len(nums)
+	for i, v := range nums {
+		if maxv > v {
+			r = i
+		} else {
+			maxv = v
+		}
+		if minv < nums[n-i-1] {
+			l = n - i - 1
+		} else {
+			minv = nums[n-i-1]
+		}
+
+	}
+	if r == -1 {
+		return 0
+	}
+	return r - l + 1
+}
+func findUnsortedSubarray2(nums []int) int {
 
 	n := len(nums)
 	pre := 0
