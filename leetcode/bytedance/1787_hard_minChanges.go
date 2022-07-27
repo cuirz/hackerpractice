14540797a64535e380bfefb4a49c9c55a7e03793
cuirz <18686224@qq.com>
2022-07-27 09:55:16 +0800

    practice

--- leetcode/bytedance/1787_hard_minChanges.go
+++ leetcode/bytedance/1787_hard_minChanges.go
@@ -3,7 +3,7 @@ package main
 import "math"
 
 //1787. 使所有区间的异或结果为零
-//给你一个整数数组 nums​​​ 和一个整数 k​​​​​ 。区间 [left, right]（left <= right）的 异或结果 是对下标位于left 和 right（包括 left 和 right ）之间所有元素进行 XOR 运算的结果：nums[left] XOR nums[left+1] XOR ... XOR nums[right] 。
+//给你一个整数数组 nums 和一个整数 k 。区间 [left, right]（left <= right）的 异或结果 是对下标位于left 和 right（包括 left 和 right ）之间所有元素进行 XOR 运算的结果：nums[left] XOR nums[left+1] XOR ... XOR nums[right] 。
 //
 //返回数组中 要更改的最小元素数 ，以使所有长度为 k 的区间异或结果等于零。
 //
@@ -29,7 +29,7 @@ import "math"
 //提示：
 //
 //1 <= k <= nums.length <= 2000
-//​​​​​​0 <= nums[i] < 2^10
+//0 <= nums[i] < 2^10
 
 //思路 动态规划
 func minChanges(nums []int, k int) int {
