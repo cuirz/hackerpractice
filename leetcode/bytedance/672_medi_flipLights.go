4d0eb424deef92948fcf7c5c0c114367ba8406e4
cuirz <18686224@qq.com>
2022-09-15 11:06:57 +0800

    practice

--- leetcode/bytedance/672_medi_flipLights.go
+++ leetcode/bytedance/672_medi_flipLights.go
@@ -25,5 +25,28 @@ package main
 //说明: 状态为: [关, 开, 关], [开, 关, 开], [关, 关, 关], [关, 开, 开].
 
 func flipLights(n int, m int) int {
+	result := make(map[int]struct{})
+	for i := 0; i < 1<<4; i++ {
+		pressArr := [4]int{}
+		sum := 0
+		for j := 0; j < 4; j++ {
+			pressArr[j] = i >> j & 1
+			sum += pressArr[j]
+		}
+		if sum%2 == m%2 && sum <= m {
+			status := pressArr[0] ^ pressArr[2] ^ pressArr[3]
+			if n >= 2 {
+				status |= (pressArr[0] ^ pressArr[1]) << 1
+			}
+			if n >= 3 {
+				status |= (pressArr[0] ^ pressArr[2]) << 2
+			}
+			if n >= 4 {
+				status |= (pressArr[0] ^ pressArr[1] ^ pressArr[3]) << 3
+			}
+			result[status] = struct{}{}
+		}
+	}
+	return len(result)
 
 }
